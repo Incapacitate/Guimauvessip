7a0a8865c4e2a8e0a28807844e59ddd6ae8d890f
Incapacitate <lhdylan@hotmail.com>
2019-02-10 13:18:05 +0100

    routes,models,controllers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def team
+  end
+  def contact
+  end
+  def index
+    # @gossip = Gossip.all.order('created_at DESC')
+    # affichage des derniers gossips
+    @gossip = Gossip.all.last(9).reverse
+    @gossip_number = Gossip.all.count
+  end
+
+end
---
+++ app/models/gossip.rb
@@ -0,0 +1,14 @@
+class Gossip < ApplicationRecord
+	validates :title,
+    presence: true
+    
+    validates :content,
+    presence: true
+
+	belongs_to :user
+	has_many :comments
+	has_many :likes
+    has_many :users, through: :likes, dependent: :destroy
+	has_many :gossips_tags
+	has_many :tags, through: :gossips_tags
+end
